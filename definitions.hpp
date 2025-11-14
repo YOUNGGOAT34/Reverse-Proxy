e5719d1551c7455c0f7a4ab6de4805729d5178d2
GOAT <lucasonyiego283@gmail.com>
2025-11-14 23:36:29 +0300

    defining the data types

---
+++ definitions.hpp
@@ -0,0 +1,22 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+//bytes
+using i8=char;
+using u8=unsigned char;
+
+//shorts
+using i16=short int;
+using u16=unsigned short int;
+
+//32-bit integers
+using i32=int;
+using u32=unsigned int;
+
+//long integers
+using i64=long int;
+using u64=unsigned long int;
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "definitions.hpp"
 
 int main(){
-     std::cout<<"Hello"<<std::endl;
+     std::cout<<"char "<<sizeof(i8)<<std::endl;
+     std::cout<<"short integer "<<sizeof(i16)<<std::endl;
+     std::cout<<"int "<<sizeof(i32)<<std::endl;
+     std::cout<<"long int "<<sizeof(i64)<<std::endl;
      return 0;
 }
