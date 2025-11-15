20b6196f1bcabdad1265e4df958f8a56b2a3d431
GOAT <lucasonyiego283@gmail.com>
2025-11-15 15:10:47 +0300

    proxy socket set up

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include "definitions.hpp"
 #include<sys/socket.h>
+#include <netinet/in.h>
 #include<string.h>
+
+const auto PORT=4221;
+
 
 int main(){
      i32 proxy_fd=socket(AF_INET,SOCK_STREAM,0);
      if(proxy_fd==-1){
         std::cout<<"error "<<strerror(errno)<<"\n";
      }
+    
+     struct sockaddr_in proxy_address;
+
+     proxy_address.sin_addr.s_addr=INADDR_ANY;
+     proxy_address.sin_family=AF_INET;
+     proxy_address.sin_port=htonl(PORT);
+     
+
      return 0;
 }
