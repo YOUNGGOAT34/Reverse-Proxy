f2d050af74db5a6f1b6d9828e3c87a3275c0df21
GOAT <lucasonyiego283@gmail.com>
2025-11-19 19:41:21 +0300

    proxy exception

--- exceptions.hpp
+++ exceptions.hpp
@@ -8,9 +8,12 @@ class ServerException:public std::runtime_error{
 };
 
 class NetworkException:public std::runtime_error{
-   
-    explicit NetworkException(const std::string& message):std::runtime_error("Server Error: "+message){}
+    explicit NetworkException(const std::string& message):std::runtime_error("Network Error: "+message){}
+};
 
+
+class ProxyException:public std::runtime_error{
+    explicit ProxyException(const std::string& message):std::runtime_error("Proxy Error: "+message){}
 };
 
 #endif
